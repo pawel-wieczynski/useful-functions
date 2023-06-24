4b4f75c7b0bb44d9d959bf843988fee2f214d3c4
pawel-wieczynski <94794405+pawel-wieczynski@users.noreply.github.com>
2023-06-24 09:00:54 +0200

    add histogram_with_densities() function

---
+++ histogram_with_densities.R
@@ -0,0 +1,46 @@
+if (!require('pacman')) install.packages('pacman')
+pacman::p_load(ggplot2, MASS)
+
+histogram_with_densities = function(data, col) {
+  
+  data[['col_scaled']] = scale(data[[col]])
+  fit = suppressWarnings(MASS::fitdistr(data[['col_scaled']], 't'))
+  df = fit$estimate['df']
+  
+  ggplot(data, aes_string('col_scaled')) +
+  
+    geom_histogram(
+      aes(y = ..density..)
+      , bins = 50
+      , color = 'white'
+      , fill = 'lightgray') +
+    
+    geom_density(size = 1, aes(color = 'Empirical')) +
+    
+    stat_function(
+      fun = dnorm
+      , aes(color = 'Normal')
+      , size = 1
+      , args = list(mean = 0, sd = 1)
+    ) +
+    
+    stat_function(
+      fun = dt
+      , aes(color = 'Student\'s t')
+      , size = 1
+      , args = list(df = df)
+    ) +
+    
+    theme_bw() +
+    theme(legend.position = 'bottom') +
+    labs(
+      x = '', y = 'Density', color = 'Distribution'
+      , title = 'Histogram with fitted densities'
+    ) -> p
+  
+  print(p)
+  
+}
+
+data = data.frame(x = rnorm(1000) + rexp(1000))
+histogram_with_densities(data, 'x')
